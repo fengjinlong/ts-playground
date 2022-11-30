c4b3e1893712d8b3a2edffd872f21475c9f0b006
werewolf <18618166564@163.com>
2022-11-30 05:41:57 +0000

    qChunk

--- src/array.ts
+++ src/array.ts
@@ -48,3 +48,49 @@ type Chunk<
     ? [[...C, f]]
     : Chunk<r, n, [...C, f]>
   : [];
+
+  // 
+type Without<T, n> = T extends []
+  ? T
+  : T extends [infer f, ...infer l]
+  ? f extends (n extends unknown[] ? n[number] : n)
+    ? Without<l, n>
+    : [f, ...Without<l, n>]
+  : never;
+type Res = Without<[1, 2], 1>; // expected to be [2]
+type Res1 = Without<[1, 2, 4, 1, 5], [1, 2]>; // expected to be [4, 5]
+type Res2 = Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>; // expected to be []
+
+// 
+type Res = IndexOf<[1, 2, 3], 2>; // expected to be 1
+type Res1 = IndexOf<[2,6, 3,8,4,1,7, 3,9], 3>; // expected to be 2
+type Res2 = IndexOf<[0, 0, 0], 2>; // expected to be -1
+type IndexOf<Arr,n,R extends unknown[]=[]> Arr extends [infer f, ...infer r]? f extends n?R['length']:IndexOf<r,n,[...R,1]>:-1
+
+type Res = Join<["a", "p", "p", "l", "e"], "-">; // expected to be 'a-p-p-l-e'
+type Res1 = Join<["Hello", "World"], " ">; // expected to be 'Hello World'
+type Res2 = Join<["2", "2", "2"], 1>; // expected to be '21212'
+type Res3 = Join<["o"], "u">; // expected to be 'o'
+
+type Join<Arr extends unknown[], j extends string | number> = Arr extends [
+  infer ff
+]
+  ? ff
+  : Arr extends [infer f extends string | number, ...infer r]
+  ? `${f}${j}${Join<r, j>}`
+  : "";
+
+  type Res11 = LastIndexOf<[1, 2, 3, 2, 1], 2>; // 3
+type Res21 = LastIndexOf<[0, 0, 0], 2>; // -1
+
+type B<n, arr extends unknown[] = []> = arr["length"] extends n
+  ? arr
+  : B<n, [...arr, 1]>;
+type LastIndexOf<Arr, n, r extends unknown[] = B<Arr["length"]>> = Arr extends [
+  ...infer l,
+  infer f
+]
+  ? f extends n
+    ? l["length"]
+    : LastIndexOf<l, n, B<l["length"]>>
+  : -1;
