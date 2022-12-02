8252facedc893a5aca32b76425cd8de77738f417
fjl <18618166564@163.com>
2022-12-02 23:46:27 +0800

    add

---
+++ src/vue.ts
@@ -0,0 +1,34 @@
+type GetPropsIns<T> = T extends new (...args: any[]) => infer P
+  ? T extends (...args: any[]) => infer R
+    ? R
+    : P
+  : T;
+
+type GetPropsArr<T, K = T> = T extends K ? GetPropsIns<T> : never;
+
+type GetPropsType<T> = T extends (infer P)[] ? GetPropsArr<P> : GetPropsIns<T>;
+
+type Props<P> = {
+  [K in keyof P]: {} extends P[K]
+    ? any
+    : P[K] extends { type: infer T }
+    ? GetPropsType<T>
+    : GetPropsType<P[K]>;
+};
+
+type Computed<C> = {
+  [K in keyof C]: C[K] extends () => infer P ? P : never;
+};
+
+type Options<P, D, C, M> = {
+  props: P;
+  data: (this: Props<P>) => D;
+  computed: C;
+  methods: M;
+} & ThisType<Props<P> & D & Computed<C> & M>;
+
+declare function VueBasicProps<P = {}, D = {}, C = {}, M = {}>(
+  options: Options<P, D, C, M>
+): any;
+
+export {};
